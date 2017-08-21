01fd8fcd1f479939e4fce463e3b909ae3432db96
Dawson Botsford <DawsonBotsford@gmail.com>
2017-08-21 00:56:07 -0700

    add more quotes

--- quotes.js
+++ quotes.js
@@ -1,4 +1,9 @@
 var quotes = [
+  'Admire someone else\'s beauty without questioning your own.',
+  'When we deny our emotions, we become a slave to them. It\'s only when we acknowledge them that we can put them in their rightful place.',
+  'So many years of education yet nobody ever taught us how to love ourselves and why it\'s important.',
+  'It\'s better to love and lose than never love at all.',
+  'Spend a little more time trying to make something of yourself and a little less time trying to impress people.',
   'Get piercings, remove them, make friends, move on, travel, stay in. Find what kind of person you are and love that person.',
   'Our purpose is never to get away from hard things. It\'s to find the hard things worth doing.',
   'Fill your life with adventures, not things. Have stories to tell, not stuff to show.',
@@ -15,7 +20,7 @@ var quotes = [
   'Be who you needed when you were younger.',
   'Be a combination of all the best people you meet, not the worst.',
   'Stop being afraid of what could go wrong and start being positive about what could go right.',
-  'A tiger does not lose sleep over the opinion of sheep.',
+  'A wolf does not lose sleep over the opinion of sheep.',
   'Be a Fruit Loop in a world of Cheerios.',
 ];
 
